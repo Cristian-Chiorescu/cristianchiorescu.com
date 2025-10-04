436b68af0d1804a578552bb3c6c91cd7cc8d1173
Cristian Chiorescu <cristian.chiores@gmail.com>
2025-10-04 15:57:23 -0600

    fix: change services id location

--- src/components/sections/services-section.tsx
+++ src/components/sections/services-section.tsx
@@ -43,37 +43,42 @@ export default function ServicesSection() {
   ];
 
   return (
-    <FadeIn delay={0.2}>
-      <section id="Services" className="flex flex-col w-full gap-8 ">
-        <h2 className="font-heading text-3xl font-bold">Services</h2>
+    <section id="Services">
+      <FadeIn delay={0.2}>
+        <div className="flex flex-col w-full gap-8 ">
+          <h2 className="font-heading text-3xl font-bold">Services</h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 ">
-          {servicesList.map((service, i) => {
-            return (
-              <FadeIn key={service.title} delay={i * 0.5 + 0.5}>
-                <Card className="gap-2 h-full flex flex-col transition hover:-translate-y-1 hover:shadow-xl dark:hover:border-muted-foreground">
-                  <CardHeader className="flex items-center gap-4">
-                    <div>{service.icon}</div>
-                    <CardTitle className="text-xl">{service.title}</CardTitle>
-                  </CardHeader>
-                  <CardContent>
-                    <ul className="mt-2 space-y-2 text-sm text-muted-foreground">
-                      {service.features.map((feature) => {
-                        return (
-                          <li key={feature} className="flex items-start gap-2">
-                            <Check className="mt-1 h-4 w-4 flex-shrink-0 text-primary"></Check>
-                            <span>{feature}</span>
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </CardContent>
-                </Card>
-              </FadeIn>
-            );
-          })}
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 ">
+            {servicesList.map((service, i) => {
+              return (
+                <FadeIn key={service.title} delay={i * 0.5 + 0.5}>
+                  <Card className="gap-2 h-full flex flex-col transition hover:-translate-y-1 hover:shadow-xl dark:hover:border-muted-foreground">
+                    <CardHeader className="flex items-center gap-4">
+                      <div>{service.icon}</div>
+                      <CardTitle className="text-xl">{service.title}</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <ul className="mt-2 space-y-2 text-sm text-muted-foreground">
+                        {service.features.map((feature) => {
+                          return (
+                            <li
+                              key={feature}
+                              className="flex items-start gap-2"
+                            >
+                              <Check className="mt-1 h-4 w-4 flex-shrink-0 text-primary"></Check>
+                              <span>{feature}</span>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </CardContent>
+                  </Card>
+                </FadeIn>
+              );
+            })}
+          </div>
         </div>
-      </section>
-    </FadeIn>
+      </FadeIn>
+    </section>
   );
 }
